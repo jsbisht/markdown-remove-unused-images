eb3d6736b9e1d968934866514f2e64f2037a8910
jsbisht <jagdeepbisht89@gmail.com>
2021-10-03 23:36:50 +0530

    added file matching and content parsing

--- fileslist.js
+++ fileslist.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const matchPattern = /\.\/[\w-.]+[?)]/g;
 
 function* walkSync(rootDirectory) {
   const files = fs.readdirSync(rootDirectory, { withFileTypes: true });
@@ -14,13 +15,11 @@ function* walkSync(rootDirectory) {
   }
 }
 
-function buildFilesList(rootDirectory, imageExtensions = ["webp"]) {
+function getFilesInDirectory(rootDirectory, imageExtensions = ["webp"]) {
   const imagesPathList = [];
   const markdownFiles = [];
 
-  for (const filePath of walkSync(
-    "/home/jsbisht/workspace/github/aws-associate/Cloud Academy - AWS Associate"
-  )) {
+  for (const filePath of walkSync(rootDirectory)) {
     const extension = filePath.substring(filePath.lastIndexOf(".") + 1);
     if (extension === "md") markdownFiles.push(filePath);
     else if (imageExtensions.includes(extension)) imagesPathList.push(filePath);
@@ -32,7 +31,33 @@ function buildFilesList(rootDirectory, imageExtensions = ["webp"]) {
   };
 }
 
-function buildUsedImagesList(markdownFiles) {}
+/**
+ * Only images with relative paths are considered
+ */
+function getImageListFromContent(content) {
+  const matchPattern = /\.\/[\w-.]+[?)]/g;
+  const imageList = [...content.matchAll(matchPattern)].flat();
+  return imageList.map((image) => image.substring(2, image.length - 1));
+}
+
+function getUsedImagesInMarkdownFile(markdownFile) {
+  try {
+    const data = fs.readFileSync(markdownFile, "utf8");
+    const list = getImageListFromContent(data);
+    return list;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function getUsedImagesList(markdownFiles) {
+  let usedImagesList = [];
+  for (const markdownFile in markdownFiles) {
+    const list = getUsedImagesInMarkdownFile(markdownFile);
+    usedImagesList = usedImagesList.concat(list);
+  }
+  return usedImagesList;
+}
 
 function deleteFiles(files, callback) {
   var i = files.length;
@@ -60,10 +85,15 @@ function deleteUnusedImages(unusedImagesPaths) {
 }
 
 function run(rootDirectory) {
-  const { imagesPaths, markdownFiles } = buildFilesList(rootDirectory);
-  const { usedImagesPaths } = buildUsedImagesList(markdownFiles);
-  const unusedImagesPaths = imagesPaths.filter(
-    (imagePath) => !usedImagesPaths.includes(imagePath)
-  );
-  deleteUnusedImages(unusedImagesPaths);
+  const { imagesPaths, markdownFiles } = getFilesInDirectory(rootDirectory);
+  const { usedImagesPaths } = getUsedImagesList(markdownFiles);
+  const unusedImagesPaths = usedImagesPaths
+    .map((usedImagesPath) => `${rootDirectory}${path.sep}${usedImagesPath}`)
+    .filter((imagePath) => !imagesPaths.includes(imagePath));
+  // deleteUnusedImages(unusedImagesPaths);
+  console.log(unusedImagesPaths);
 }
+
+run(
+  "/home/jsbisht/workspace/github/aws-associate/Cloud Academy - AWS Associate"
+);
